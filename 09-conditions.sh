c46db837c1e7d1b6c6c1ed9adc0d369ee7fb5f74
Surya Prakash <suryasrinivas765@gmail.com>
2025-05-21 22:30:08 +0530

    variables

---
+++ 09-conditions.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+Number=1
+
+# -gt --> greater than
+# -lt --> less than
+# -eq --> equal
+# -ne --> not equal
+
+if($Number -lt 100)
+then
+    echo "given number is lessthan 100"
+else
+    ech "given number is greater thean 100"
+fi
