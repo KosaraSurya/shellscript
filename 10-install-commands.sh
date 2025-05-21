55574ac6bee0b12d5c16e636a9391eec6e2e312d
Surya Prakash <suryasrinivas765@gmail.com>
2025-05-22 00:30:22 +0530

    variables

--- 10-install-commands.sh
+++ 10-install-commands.sh
@@ -4,8 +4,22 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo "please run as root user"
+    echo "ERROR : Please run as root acess"
 else
     echo "running with root access"
+fi
+
+dnf list installed mysql
+if [ $? -eq 0 ]
+then 
+    echo "mysql is already installed in your system"
+else
+    echo  "mysl is goin to install"
     dnf install mysql -y
+    if [ $? -eq 0 ]
+    then 
+        echo "installed successfully"
+    else   
+        echo "ERROR : installaion failurre"
+        exit 1
 fi
