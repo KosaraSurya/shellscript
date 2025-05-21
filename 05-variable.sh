5a1a96db5597c2772b4ade2cec9737856d929a0f
Surya Prakash <suryasrinivas765@gmail.com>
2025-05-21 21:33:22 +0530

    variables

--- 05-variable.sh
+++ 05-variable.sh
@@ -2,6 +2,6 @@
 
 echo "enter your pin"
 
-read  PIN  #here pin is a variable
+read -s PIN  #here pin is a variable and if you remove -s you can see the pin
 
 #echo "your pin is $PIN"    #if uncoment this line you can see the PIN is entered.
