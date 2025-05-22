1c2177e509b4a818f46f4e9adf646679a205a359
Surya Prakash <suryasrinivas765@gmail.com>
2025-05-23 00:35:56 +0530

    functions

---
+++ 12-colors.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+
+echo -e "\e[31m Hello world \e[0m"      #Here 31 is red colour, 0 is no color
+
+echo "no colours"  #Here if we don't provide 0 at the end of the life then the previously give color will add to remaining lines
