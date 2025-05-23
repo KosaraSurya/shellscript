f9509e2ac7b98004ad14e315a0771ed58922ce2c
Surya Prakash <suryasrinivas765@gmail.com>
2025-05-24 02:08:26 +0530

    functions

--- 14-logs.sh
+++ 14-logs.sh
@@ -8,6 +8,8 @@ LOG_FOLDER=/var/log/shellpractice
 SCRIPT_NAME=$(echo $0 | cut -d "." f1)
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 
+mkdir -p $LOG_FOLDER    #-p will check whether dir is there or not, if it not exits it will create the folder.
+
 if [ $USERID -ne 0 ]
 then
     echo -e "$R ERROR : Please proceed with root acccess$N"
