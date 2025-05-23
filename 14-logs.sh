bfb4e713038ffe9c620be4de4284a7bc7e8578a2
Surya Prakash <suryasrinivas765@gmail.com>
2025-05-24 01:59:59 +0530

    functions

--- 14-logs.sh
+++ 14-logs.sh
@@ -10,38 +10,38 @@ LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 
 if [ $USERID -ne 0 ]
 then
-    echo "$R ERROR : Please proceed with root acccess$N"
+    echo -e "$R ERROR : Please proceed with root acccess$N"
     exit 1
 else
-    echo "$G Welcome to the root user$N" &>>LOG_FILE
+    echo -e "$G Welcome to the root user$N" &>>LOG_FILE
 fi
 
 VALIDATE(){
     if [ $1 -ne 0 ] 
     then
-        echo "$R ERROR : $2 installation was failed please check $N" &>>LOG_FILE
+        echo -e "$R ERROR : $2 installation was failed please check $N" &>>LOG_FILE
         exit 1
     else
-        echo "$G $2 installation was successful $N"
+        echo -e "$G $2 installation was successful $N"
     fi
 }
 
 dnf list installed mysql
 if [ $? -ne 0 ]
 then
-    echo "$Y mysql is not instlled,please proceed to install $N"
+    echo -e "$Y mysql is not instlled,please proceed to install $N"
     dnf install mysql -y &>>LOG_FILE
     VALIDATE $? "MYSQL"
 else
-    echo "$G mysql is already installed $N"
+    echo -e "$G mysql is already installed $N"
 fi
 
 dnf list installed python3
 if [ $? -ne 0 ]
 then
-    echo "$Y python3 is not instlled,please proceed to install $N"
+    echo -e "$Y python3 is not instlled,please proceed to install $N"
     dnf install python3 -y &>>LOG_FILE
     VALIDATE $? "python3"
 else
-    echo "$G mypython3sql is already installed $N"
+    echo -e "$G mypython3sql is already installed $N"
 fi
