3f2d968a1b89d5ab4614a1a25fa47571c377fe88
Surya Prakash <suryasrinivas765@gmail.com>
2025-05-24 02:48:06 +0530

    functions

--- 14-logs.sh
+++ 14-logs.sh
@@ -16,12 +16,15 @@ then
     exit 1
 else
     echo -e "$G Welcome to the root user$N" &>>$LOG_FILE
+    #By using &>>$LOG_FILE command will not display the elog on the screen, it will only saves in file.
+    echo -e "$G Welcome to the root user$N" | tee -a $LOG_FILE 
+    #Tee command will display th elog on the screen as well as saves in file.
 fi
 
 VALIDATE(){
     if [ $1 -ne 0 ] 
     then
-        echo -e "$R ERROR : $2 installation was failed please check $N" &>>$LOG_FILE
+        echo -e "$R ERROR : $2 installation was failed please check $N" | tee -a $LOG_FILE 
         exit 1
     else
         echo -e "$G $2 installation was successful $N"
@@ -32,7 +35,7 @@ dnf list installed mysql
 if [ $? -ne 0 ]
 then
     echo -e "$Y mysql is not instlled,please proceed to install $N"
-    dnf install mysql -y &>>$LOG_FILE
+    dnf install mysql -y | tee -a $LOG_FILE 
     VALIDATE $? "MYSQL"
 else
     echo -e "$G mysql is already installed $N"
@@ -42,7 +45,7 @@ dnf list installed python3
 if [ $? -ne 0 ]
 then
     echo -e "$Y python3 is not instlled,please proceed to install $N"
-    dnf install python3 -y &>>$LOG_FILE
+    dnf install python3 -y | tee -a $LOG_FILE
     VALIDATE $? "python3"
 else
     echo -e "$G mypython3sql is already installed $N"
