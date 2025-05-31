81f1ef081282244dc178f880bc4857fcebd638dd
Surya Prakash <suryasrinivas765@gmail.com>
2025-05-31 20:52:56 +0530

    practice

--- 17.set.sh
+++ 17.set.sh
@@ -1,5 +1,7 @@
 #!bin/bash
 
+set -e  #setting error
+
 echo "hello world"
-echooooo "good morning"
-echo "welcome to hyderabad"
+echooooo "good morning" #generally shell script will not stop executing after error.
+echo "welcome to hyderabad" #If we want to stop executing at the time of error, we have to mention set -e
