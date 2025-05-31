5546b45acd0040e8d33a7d40aabef7cce5aa0ad5
Surya Prakash <suryasrinivas765@gmail.com>
2025-05-31 21:13:39 +0530

    practice

--- 17.set.sh
+++ 17.set.sh
@@ -1,7 +1,17 @@
 #!bin/bash
 
-set -e  #setting error
+#setting error, If we want to stop executing at the time of error, we have to mention set -e
+set -e
+#But set command will not give proper error discription to get it, we will use trap command along with set command
+
+failure(){
+    echo "failed at $1 $2"
+}
+
+trap 'failure "${LINENO}" "{BASH_COMMAND}"' ERR
+
+#Here LINENO, BASH_COMMAND are special variables and ERR is the error signal
 
 echo "hello world"
 echooooo "good morning" #generally shell script will not stop executing after error.
-echo "welcome to hyderabad" #If we want to stop executing at the time of error, we have to mention set -e
+echo "welcome to hyderabad" #but 
